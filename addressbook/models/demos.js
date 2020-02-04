f70afe148c2fcd936d95690856a62b70f2cff3cd
Ricardo Sotolongo <rickey.soto@gmail.com>
2020-02-04 18:03:07 -0500

    added model.

---
+++ addressbook/models/demos.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+//set up db object model
+const contact = new Schema({
+    lastName: { type: String, required: true },
+    firstName: { type: String, required: true },
+    email: { type: String, required: false },
+    phoneNumber: { type: String, required: true },
+    birthDate: { type: String, required: false },
+    address: { type: String, required: false }
+})
+
+module.exports = mongoose.model("Contacts", contact);
