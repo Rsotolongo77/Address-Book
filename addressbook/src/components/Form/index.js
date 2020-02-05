941076b62d3b18e09061e499af4bb4fa1d01c5df
Ricardo Sotolongo <rickey.soto@gmail.com>
2020-02-05 08:55:31 -0500

    form functions created.

---
+++ addressbook/src/components/Form/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+//export functions to be used in form on contact page
+export function Input(props) {
+    return (
+        <div className="form-group">
+            <input className="form-control" {...props} />
+        </div>
+    );
+}
+
+export function TextArea(props) {
+    return (
+        <div className="form-group">
+            <textarea className="form-control" rows="20" {...props} />
+        </div>
+
+    );
+}
+
+export function FormBtn(props) {
+    return (
+        <button {...props} className="btn" id="formBtn">
+            {props.children}
+        </button>
+    );
+}
+
+export function SearchBtn(props) {
+    return (
+        <button {...props} className="btn" id="searchBtn">
+            {props.children}
+        </button>
+    );
+}
