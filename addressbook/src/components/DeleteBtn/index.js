76ee57cf435e5e2de18eb94fd55aa98a01ce49a2
Ricardo Sotolongo <rickey.soto@gmail.com>
2020-02-05 08:32:42 -0500

    created delete btn.

---
+++ addressbook/src/components/DeleteBtn/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+//delete btn with props passed in
+function DeleteBtn(props) {
+    return (
+        <button className="delete-btn" id="deleteBtn" {...props}>Delete Contact</button>
+    );
+}
+
+export default DeleteBtn;
