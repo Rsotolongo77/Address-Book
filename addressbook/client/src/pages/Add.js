109d86fb14ec022bf3c7fcf97a1e466f75210d4c
Ricardo Sotolongo <rickey.soto@gmail.com>
2020-02-07 13:58:50 -0500

    add page done.

--- addressbook/client/src/pages/Add.js
+++ addressbook/client/src/pages/Add.js
@@ -55,59 +55,62 @@ class AddContact extends Component {
     render() {
         return (
             <Container fluid>
-                <Row>
-                    <Col size="md-6">
+                <row>
+                    <Col size="md-12">
                         <Jumbotron>
-                            <h1>Address Book Challenge</h1>
-                            <Link to="/">← Back to Address Book</Link>
+                            <h1 id="header">Michael Scott Paper Company</h1>
+                            <h1 className="display-4">Directory</h1>
+                            <Link to="/" id="addLink">← Back to Address Book</Link>
                         </Jumbotron>
                         <SearchJumbo />
-                        <form>
-                            <Input
-                                value={this.state.lastName}
-                                onChange={this.handleInputChange}
-                                name="lastName"
-                                placeholder="Last Name (required)" />
-                            <Input
-                                value={this.state.firstName}
-                                onChange={this.handleInputChange}
-                                name="firstName"
-                                placeholder="First Name (required)" />
-                            <Input
-                                value={this.state.email}
-                                onChange={this.handleInputChange}
-                                name="email"
-                                placeholder="Email" />
-                            <Input
-                                value={this.state.phoneNumber}
-                                onChange={this.handleInputChange}
-                                name="phoneNumber"
-                                placeholder="Phone Number" />
-                            <Input
-                                value={this.state.birthDate}
-                                onChange={this.handleInputChange}
-                                name="birthDate"
-                                placeholder="Birth Date" />
-                            <Input
-                                value={this.state.address}
-                                onChange={this.handleInputChange}
-                                name="address"
-                                placeholder="Address" />
-                            <TextArea
-                                value={this.state.notes}
-                                onChange={this.handleInputChange}
-                                name="notes"
-                                placeholder="Notes" />
+                        <div>
+                            <form>
+                                <Input
+                                    value={this.state.lastName}
+                                    onChange={this.handleInputChange}
+                                    name="lastName"
+                                    placeholder="Last Name (required)" />
+                                <Input
+                                    value={this.state.firstName}
+                                    onChange={this.handleInputChange}
+                                    name="firstName"
+                                    placeholder="First Name (required)" />
+                                <Input
+                                    value={this.state.email}
+                                    onChange={this.handleInputChange}
+                                    name="email"
+                                    placeholder="Email" />
+                                <Input
+                                    value={this.state.phoneNumber}
+                                    onChange={this.handleInputChange}
+                                    name="phoneNumber"
+                                    placeholder="Phone Number" />
+                                <Input
+                                    value={this.state.birthDate}
+                                    onChange={this.handleInputChange}
+                                    name="birthDate"
+                                    placeholder="Birth Date" />
+                                <Input
+                                    value={this.state.address}
+                                    onChange={this.handleInputChange}
+                                    name="address"
+                                    placeholder="Address" />
+                                <TextArea
+                                    value={this.state.notes}
+                                    onChange={this.handleInputChange}
+                                    name="notes"
+                                    placeholder="Notes" />
 
-                            <FormBtn
-                                //don't run if condition not met
-                                disabled={!(this.state.lastName && this.state.firstName)}
-                                onClick={this.handleFormSubmit}>
-                                Submit Contact
-                  </FormBtn>
-                        </form>
+                                <FormBtn
+                                    //don't run if condition not met
+                                    disabled={!(this.state.lastName && this.state.firstName)}
+                                    onClick={this.handleFormSubmit}>
+                                    Submit Contact
+                        </FormBtn>
+                            </form>
+                        </div>
                     </Col>
-                </Row>
+                </row>
             </Container>
         );
     }
