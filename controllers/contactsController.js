61c4c50dec5c6b8f0a2eeb7cc4c14f106e181a3b
Ricardo Sotolongo <rickey.soto@gmail.com>
2020-04-15 07:38:11 -0400

    updated functions to es6.

--- controllers/contactsController.js
+++ controllers/contactsController.js
@@ -2,39 +2,39 @@ const db = require("../models");
 
 //methods to be applied to model 
 module.exports = {
-  findAll: function (req, res) {
+  findAll: (req, res) => {
     db.Contact
       .find(req.query)
       .sort({ lastName: 1 })
       .then(data => res.json(data))
       .catch(err => res.status(422).json(err))
   },
-  findById: function (req, res) {
+  findById: (req, res) => {
     db.Contact
       .findById(req.params.id)
       .then(data => res.json(data))
       .catch(err => res.status(422).json(err));
   },
-  findByCategory: function (req, res) {
+  findByCategory: (req, res) => {
     db.Contact
       //search by first or lastname
       .find({ $or: [{ lastName: req.params.category }, { firstName: req.params.category }] })
       .then(data => res.json(data))
       .catch(err => res.status(422).json(err));
   },
-  create: function (req, res) {
+  create: (req, res) => {
     db.Contact
       .create(req.body)
       .then(data => res.json(data))
       .catch(err => res.status(422).json(err))
   },
-  update: function (req, res) {
+  update: (req, res) => {
     db.Contact
       .findOneAndUpdate({ _id: req.params.id }, req.body)
       .then(data => res.json(data))
       .catch(err => res.status(422).json(err));
   },
-  remove: function (req, res) {
+  remove: (req, res) => {
     db.Contact
       .findById(req.params.id)
       .then(data => data.remove())
