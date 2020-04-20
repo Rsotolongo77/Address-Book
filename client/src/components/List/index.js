4740be5dc06c71c90cf35a7b9d7ad07d416a49c8
Ricardo Sotolongo <rickey.soto@gmail.com>
2020-04-20 10:16:15 -0400

    minor code structure adjustment for better readability to list component.

--- client/src/components/List/index.js
+++ client/src/components/List/index.js
@@ -4,12 +4,16 @@ import "./list.css";
 //export functions to pass children onto list elements
 const List = props => (
   <div className="list-overflow-container">
-    <ul className="list-group">{props.children}</ul>
+    <ul className="list-group">
+      {props.children}
+    </ul>
   </div>
 );
 
 const ListItem = props => (
-  <li id="listItem" className="list-group-item">{props.children}</li>
+  <li id="listItem" className="list-group-item">
+    {props.children}
+  </li>
 );
 
 export { List, ListItem };
